27df5973ccf31410508e178aac0b96e9d64fbf00
Chris Nuernberger <chris@thinktopic.com>
2016-11-16 14:28:50 -0700

    Datatype library from cortex pulled over.

--- project.clj
+++ project.clj
@@ -3,4 +3,9 @@
   :url "http://github.com/thinktopic/think.datatype"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [net.mikera/vectorz-clj "0.45.0"]
+                 [net.mikera/core.matrix "0.56.0"]]
+
+  :java-source-paths ["java"]
+  )
--- test/think/datatype_test.clj
+++
@@ -1,7 +0,0 @@
-(ns think.datatype-test
-  (:require [clojure.test :refer :all]
-            [think.datatype :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))

a8a85ceabebcd021ba53cf094f0b8fa6212b7129
Chris Nuernberger <chris@thinktopic.com>
2017-02-09 11:50:24 -0700

    Release 0.3.9

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thinktopic/think.datatype "0.3.8"
+(defproject thinktopic/think.datatype "0.3.9"
   :description "Library for efficient manipulation of contiguous mutable containers of primitive datatypes."
   :url "http://github.com/thinktopic/think.datatype"
   :license {:name "Eclipse Public License"

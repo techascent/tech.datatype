aa385e675cfdbab80f3a6404c96a083603da42b4
Chris Nuernberger <chris@thinktopic.com>
2017-05-03 13:06:55 -0600

    back to snap.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thinktopic/think.datatype "0.3.11"
+(defproject thinktopic/think.datatype "0.3.12-SNAPSHOT"
   :description "Library for efficient manipulation of contiguous mutable containers of primitive datatypes."
   :url "http://github.com/thinktopic/think.datatype"
   :license {:name "Eclipse Public License"

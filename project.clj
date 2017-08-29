b4e625119f872f8052ece4c858f04c7f79b3a895
Chris Nuernberger <chris@thinktopic.com>
2017-08-29 10:46:28 -0600

    upgrading dependencies

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject thinktopic/think.datatype "0.3.15"
+(defproject thinktopic/think.datatype "0.3.16-SNAPSHOT"
   :description "Library for efficient manipulation of contiguous mutable containers of primitive datatypes."
   :url "http://github.com/thinktopic/think.datatype"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
-                 [net.mikera/vectorz-clj "0.46.0"]
-                 [net.mikera/core.matrix "0.59.0"]]
+                 [net.mikera/vectorz-clj "0.47.0"]
+                 [net.mikera/core.matrix "0.60.3"]]
 
 
   :java-source-paths ["java"]

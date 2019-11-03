c71714b7a43379d4c58e514366b7a11e524942ff
Chris Nuernberger <chris@techascent.com>
2019-11-03 13:32:56 -0700

    4.35

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.datatype "4.34"
+(defproject techascent/tech.datatype "4.35"
   :description "Library for efficient manipulation of contiguous mutable containers of primitive datatypes."
   :url "http://github.com/tech-ascent/tech.datatype"
   :license {:name "Eclipse Public License"

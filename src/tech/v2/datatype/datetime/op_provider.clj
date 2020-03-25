6d1446673507c74e0885d28e385e2adbf568a12c
Chris Nuernberger <chris@techascent.com>
2020-03-25 13:08:38 -0600

    Removing these unused files.

--- src/tech/v2/datatype/datetime/op_provider.clj
+++
@@ -1,7 +0,0 @@
-(ns tech.v2.datatype.datetime.op-provider
-  (:require [tech.v2.datatype.operation-provider :as op-provider]
-            [tech.v2.datatype.argtypes :as argtypes]
-            [tech.v2.datatype.base :as base]
-            [tech.v2.datatype.casting :as casting]
-            [tech.v2.datatype.object-datatypes :as obj-dtypes])
-  (:import [java.time LocalDateTime ZoneOffset]))

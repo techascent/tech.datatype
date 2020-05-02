b6c88ae8b902e47ed4a9916b8e6a2db63d751ff4
Chris Nuernberger <chris@techascent.com>
2020-05-01 19:08:47 -0600

    Added a few more perf tests (Arrays/sort vs. fastutil IntArrays quicksort and parallelquicksort

--- test/tech/v2/datatype/simple_ops_perf_test.clj
+++ test/tech/v2/datatype/simple_ops_perf_test.clj
@@ -2,8 +2,11 @@
   (:require [tech.v2.datatype :as dtype]
             [tech.v2.datatype.typecast :as typecast]
             [tech.v2.datatype.functional :as dfn]
+            [tech.v2.datatype.comparator :as comp]
             [tech.parallel.for :as parallel-for]
-            [criterium.core :as crit]))
+            [criterium.core :as crit])
+  (:import [it.unimi.dsi.fastutil.ints IntArrays IntComparator]
+           [java.util Comparator Arrays]))
 
 
 (set! *unchecked-math* :warn-on-boxed)
@@ -41,4 +44,28 @@
      idx
      (long n-elems)
      (.write wtr
-           idx (.read rdr idx)))))
+             idx (.read rdr idx)))))
+
+(def obj-data (object-array (shuffle (range n-elems))))
+
+(defn arrays-comparator-sort
+  [ary-data]
+  (let [test-comp (comparator <)]
+    (Arrays/sort ary-data test-comp)
+    (dtype/make-container :java-array :int64 (dtype/->reader ary-data :int64))))
+
+
+(def int-data (int-array (shuffle (range n-elems))))
+
+(defn int-arrays-quicksort
+  []
+  (let [test-comp (comp/make-comparator
+                   :int32 (- lhs rhs))]
+    (IntArrays/quickSort int-data test-comp)))
+
+
+(defn parallel-int-arrays-quicksort
+  []
+  (let [test-comp (comp/make-comparator
+                   :int32 (- lhs rhs))]
+    (IntArrays/parallelQuickSort int-data test-comp)))

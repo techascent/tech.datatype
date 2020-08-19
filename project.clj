22ef9a761cb7a809ced29df94b48195e079f2a25
Chris Nuernberger <chris@techascent.com>
2020-08-19 07:48:17 -0600

    Upgrading test.check to latest.

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
                   :exclusions [org.clojure/test.check]]
                  [org.clojure/math.combinatorics   "0.1.6"]
                  [org.clojure/math.numeric-tower   "0.0.4"]
-                 [org.clojure/test.check           "1.0.0"]
+                 [org.clojure/test.check           "1.1.0"]
                  [org.apache.commons/commons-math3 "3.6.1"]
                  [primitive-math                   "0.1.6"]
                  [camel-snake-kebab                "0.4.0"]
